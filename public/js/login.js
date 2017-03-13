1c656d3fa1d44159c82be93dcec01a90534f16a0
Jacob Gilis <gilis.jakub@gmail.com>
2017-03-13 15:11:37 +0100

    Cleanup

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,3 @@
-//
-// =============================================================================
-// Login vue file
-// =============================================================================
-//
-
-// ================================================
-// Function which crates unique id
-// ================================================
-
 function guid() {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
@@ -17,72 +7,39 @@ function guid() {
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4();
 }
 
-// ================================================
-// Init of root vue instance
-// ================================================
-
 var login_form = new Vue({
-    el: '#login', // root element
-
-    // ================================================
-    // Properties used later in code
-    // ================================================
-
+    el: '#login',
     props: {
-        user_login: {default: ''}, // user login
-        display_errors: {default: false}, // displaying errors or not
-        error_message: {default: ''} // current error message
+        user_login: {default: ''},
+        display_errors: {default: false},
+        error_message: {default: ''}
     },
-
-    // =============================================================
-    // Mounted function triggers asap vue is init
-    // Checking if user already is saved in local storage
-    // If so he will be redirected to chat else he will see login
-    // =============================================================
-
     mounted: function () {
         this.redirect_to_chat();
     },
-
-    // ================================================
-    // Computed properties are changing
-    // ================================================
-
     computed: {
         has_errors: {
-
-            // similar to get set validation in c# MVC
-
             get: function () {
                 if (this.user_login == '') {
                     this.error_message = 'Please enter your nickname';
                 }
-                return this.error_message; // returns error if exist
+                return this.error_message;
             },
             set: function (newValue) {
                 this.error_message = newValue;
             }
         }
     },
-
-    // ================================================
-    // Methods used in login
-    // ================================================
-
     methods: {
-        // Login method. If user is not logged he will be added to local storage
         login: function () {
-            //checking if user has username
             if (this.user_login != '') {
                 sessionStorage.setItem('mandatory_chat_user', this.user_login);
                 sessionStorage.setItem('mandatory_chat_user_id', guid());
                this.redirect_to_chat();
             }else{
-                // if not display error
                 this.display_errors = true;
             }
         },
-        // Redirect method checks if user is logged nad redirects if its necessery to chat
         redirect_to_chat: function () {
             if(sessionStorage.getItem("mandatory_chat_user") && sessionStorage.getItem("mandatory_chat_user_id")){
                 document.location.href = "/chat";
